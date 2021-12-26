7fae7971813216e07f4529f4c2396599d7f06951
mohachmadim <72310049+mohachmadim@users.noreply.github.com>
2021-12-26 20:20:29 +0100

    created schema and connected to db

--- task-management/server/db/index.js
+++ task-management/server/db/index.js
@@ -1,3 +1,12 @@
 /**
  * Connect to the Database with Mongoose here
  */
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+// open a connection to the test database on our locally running instance of MongoDB.
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+}
